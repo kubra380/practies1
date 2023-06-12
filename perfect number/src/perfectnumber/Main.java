5af6e95336850db1be1ec5fefba2b1851ca1efd1
kübraonmaz <101880055+kubra380@users.noreply.github.com>
2023-06-12 20:59:08 +0300

    Update Main.java

--- perfect number/src/perfectnumber/Main.java
+++ perfect number/src/perfectnumber/Main.java
@@ -1,6 +1,6 @@
 package perfectnumber;
 import java.util.Scanner;
-
+//Bir sayının kendisi hariç pozitif tam sayı çarpanları (kalansız bölen sayıların) toplamı kendisine eşit olan sayıya mükemmel sayı denir.
 public class Main {
 
 	public static void main(String[] args) {
